c046e6797816ca9a35e5b2a852d74778afe0dc3f
kastrio_it <wofyd465@gmail.com>
2024-03-01 23:21:13 +0900

    ch3 01 start

---
+++ Ch3/SpinUnContainer/MessagesApp/src/main/java/app/messages/Application.java
@@ -0,0 +1,13 @@
+package app.messages;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessageService messageService = context.getBean(MessageService.class);
+        messageService.save("Hello, Spring?");
+    }
+
+}
