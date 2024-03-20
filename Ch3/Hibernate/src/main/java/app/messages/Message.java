1a9a168bf86fe4f7097759dccca57d8b80a2718d
wd48 <wofyd465@gmail.com>
2024-03-20 13:06:39 +0900

    add a hibernate

---
+++ Ch3/Hibernate/src/main/java/app/messages/Message.java
@@ -0,0 +1,59 @@
+package app.messages;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @Column(name = "text", nullable = false, length = 128)
+  private String text;
+
+  @Column(name = "created_date", nullable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+
+  public Message() {}
+
+  public Message(String text) {
+    this.text = text;
+    this.createdDate = new Date();
+  }
+
+  public Message(int id, String text, Date createdDate) {
+    this.id = id;
+    this.text = text;
+    this.createdDate = createdDate;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Message message = (Message) o;
+    return Objects.equals(id, message.id);
+  }
+}
---
+++ Ch3/Hibernate/src/main/java/app/messages/MessageRepository.java
@@ -0,0 +1,21 @@
+package app.messages;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRepository {
+
+  private SessionFactory sessionFactory;
+
+  public MessageRepository(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
+  public Message saveMessage(Message message) {
+    Session session = sessionFactory.openSession();
+    session.save(message);
+    return message;
+  }
+}
